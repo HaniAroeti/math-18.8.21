97a980ead1e756ff700f7dc4e032a3407d7e62ed
HaniAroeti <haniaroeti@gmail.com>
2021-08-21 21:51:21 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,93 @@
+// תרגילים מחלקה Math
+// 1.	צרו פונקציה שמחזירה מספר רנדומלי בין 0 ל 99.
+
+// function randomNumber() {
+//     var randomNumber1 = Math.random ()*99 + 1;
+//     return(randomNumber1);
+    
+// }
+// randomNumber ();
+
+// 2.	צרו פונקציה שמחזירה מספר רנדומלי בין 0 ל 9999.
+
+// function randomNumber() {
+//     randonNumber2 = (Math.random ()*9999+1)
+//     return(randonNumber2);
+//     }
+//     randomNumber();
+
+// 3.	צרו פונקציה שמחזירה מספר רנדומלי בין 0 ל 54.
+
+// function randomNumber() {
+//     randonNumber3 = (Math.random ()*54+1)
+//     return(randonNumber3);
+//     }
+//     randomNumber();
+
+// 4.	צרו פונקציה שמקבלת מספר מהמשתמש 
+// ומחזירה מספר רנדומלי מ0 ועד המספר.
+
+//  function getRandonInput(randonNumberInput) {
+//      randonNumber4 = (Math.random ()*randonNumberInput)
+//         return(randonNumber4);
+//      }
+//      getRandonInput("5");
+
+
+// 5.	צרו פונקציה שמקבלת מהמשתמש מספר בין 0 ל9 ובודקת האם המספר זהה למספר רנדומלי.
+
+// function getRandonInput(randonNumberInput) {
+//     var x = 9;
+//     randonNumber5 = (Math.random ()*9+1)
+//     console.log(randonNumber5);
+//     if(randonNumberInput !== randonNumber5){
+//         console.log("try again");
+//     }
+//     else{
+//     console.log("correct number");
+//     console.log(randonNumber5);
+
+//     }
+//     }
+//     getRandonInput(9);
+
+
+
+// 6.	צרו פונקציה שמקבלת מהמשתמש מספר בין 0 ל99 ובודקת האם המספר זהה למספר רנדומלי.
+
+// function getRandonInput(randonNumberInput) {
+//     var x = 9;
+//     randonNumber6 = (Math.random ()*99+1)
+//     console.log(randonNumber6);
+//     if(randonNumberInput !== randonNumber5){
+//         console.log("try again");
+//     }
+//     else{
+//     console.log("correct number");
+//     console.log(randonNumber6);
+
+//     }
+//     }
+//     getRandonInput(99);
+
+
+// 7.	צרו פונקציה שמקבלת 
+// מהמשתמש שם פרטי ומספר בין 0 ל9, 
+// הפונקציה מדפיסה למשתמש עם השם, הודעה האם 
+// הוא זכה ואת המספר שיצא בהגרלה 
+
+// function getRandonInput( yourName,randonNumberInput) {
+//     var min = Math.ceil (9);
+//     var max = Math.floor (0);
+//     randonNumber7 = (Math.random ()(max - min +1)+ min)
+//     console.log(randonNumber7);
+//     if(randonNumberInput !== randonNumber5){
+//         console.log("try again");
+//     }
+//     else{
+//     console.log("correct number");
+//     console.log(randonNumber7);
+
+//     }
+//     }
+//     getRandonInput(99);
